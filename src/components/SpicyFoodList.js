13511ceb19b8efb982ee56c53bc9952b1f35fe75
SirJimKe <jimmymutuku1@gmail.com>
2022-09-22 12:47:18 +0300

    Done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,19 +6,42 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    setFoods(newFood);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleLiClick() {
+    const newFoodArray = foods.filter((food) => food.id !== food.id);
+    console.log(newFoodArray)
+  }
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={()=>handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
     </div>
   );
 }
